b903ac966efbb11e26edcbbbdad60db2ee07c276
Azeemuddin Sunkesula <143476100+Azeemuddin1306@users.noreply.github.com>
2024-06-19 11:10:34 +0530

    Add files via upload

--- Last Digit Check.java
+++ Last Digit Check.java
@@ -1,16 +1,29 @@
-//Take integer N, print Yes if the last digit is 0, No in every other case
+//Take a integer N and print Yes if last digit is 7 and No in every other case
 import java.util.Scanner;
-class Last_digit{
+class Last_Digit_Is_7{
     public static void main(String[]args){
-        Scanner scan = new Scanner (System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter: ");
         int N=scan.nextInt();
-        if(N%10==0){
-            System.out.println("Yes");
+        if((N+3)%10==0){
+            System.out.print("Yes");
         }
         else{
-            System.out.println("No");
+            System.out.print("No");
         }
-        
+    }
+}
+
+
+//Using Ternary operator
+//Take a integer N and print Yes if last digit is 7 and No in every other case
+import java.util.Scanner;
+class Last_Digit_Is_7{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter: ");
+        int N=scan.nextInt();
+        String result=((N+3)%10)==0 ? "Yes":"No";
+        System.out.print(result);
     }
 }
