d4f6c862b37a3039e150381e13d1f19b7f192a91
Azeemuddin Sunkesula <143476100+Azeemuddin1306@users.noreply.github.com>
2024-06-21 22:10:26 +0530

    Add files via upload

---
+++ Printing pattern.java
@@ -0,0 +1,11 @@
+//printing stars as a square
+public class Pattern_Printing{
+    public static void main(String[]args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
