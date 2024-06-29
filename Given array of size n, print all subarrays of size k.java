df1202567fe8e5da03aed4e30bc169f09fb8d752
Azeemuddin Sunkesula <143476100+Azeemuddin1306@users.noreply.github.com>
2024-06-29 12:37:13 +0530

    Add files via upload

--- Given array of size n, print all subarrays of size k.java
+++ Given array of size n, print all subarrays of size k.java
@@ -2,11 +2,16 @@
 import java.util.Scanner;
 class Arrays{
     static void SubArrays(int[]ar,int size){
-        for(int i=0;i<ar.length-size;i++){
+        for(int i=0;i<=ar.length-size;i++){
             for(int j=i;j<i+size;j++){
                 System.out.print(ar[j]+" ");
             }
             System.out.println();
+        }
+    }
+    static void PrintSubArrays(int[]ar){
+        for(int size=0;size<=ar.length;size++){
+            SubArrays(ar,size);
         }
     }
     public static void main(String[]args){
@@ -16,7 +21,7 @@ class Arrays{
         for(int i=0;i<ar.length;i++){
             ar[i]=scan.nextInt();
         }
-        int size=scan.nextInt();
-        SubArrays(ar,size);
+        //int size=scan.nextInt();
+        PrintSubArrays(ar);
     }
 }
